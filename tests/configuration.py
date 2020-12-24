70873462945dd020073dd629079efe2281d28481
Kyle King <KyleKing@users.noreply.github.com>
2020-12-24 10:25:03 -0500

    test: new test for changelog

---
+++ tests/configuration.py
@@ -0,0 +1,12 @@
+"""Global variables for testing."""
+
+from pathlib import Path
+
+TEST_DIR: Path = Path(__file__).parent
+"""Path to the `test` directory that contains this file and all other tests."""
+
+TEST_DATA_DIR: Path = TEST_DIR / 'data'
+"""Path to subdirectory with test data within the Test Directory."""
+
+PATH_TEST_CHANGELOG: Path = TEST_DATA_DIR / 'CHANGELOG.md'
+"""Path to the test CHANGELOG file."""
--- tests/test_cz_legacy.py
+++ tests/test_cz_legacy.py
@@ -1,9 +1,13 @@
 """Test the cz_legacy module."""
 
 import pytest
+from commitizen import changelog
 from commitizen.config import BaseConfig
 
 from cz_legacy import discover_this
+
+from .configuration import PATH_TEST_CHANGELOG
+from .conftest import CHANGELOG_TREE
 
 
 def test_missing_legacy_map():
@@ -23,3 +27,25 @@ def test_message(config, messages):
 
     assert message == messages.expected
 
+
+def test_generate_tree_from_commits(config, gitcommits, tags):
+    """Test generating the changelog tree."""
+    cz = discover_this(config)
+    parser = cz.commit_parser
+    changelog_pattern = cz.bump_pattern
+
+    tree = changelog.generate_tree_from_commits(gitcommits, tags, parser, changelog_pattern)  # act
+
+    assert tuple(tree) == CHANGELOG_TREE
+
+
+def test_render_changelog(config, gitcommits, tags):  # , changelog_content):
+    """Test generating the changelog content."""
+    cz = discover_this(config)
+    parser = cz.commit_parser
+    changelog_pattern = cz.bump_pattern
+    tree = changelog.generate_tree_from_commits(gitcommits, tags, parser, changelog_pattern)
+
+    result = changelog.render_changelog(tree)
+
+    assert result == PATH_TEST_CHANGELOG.read_text()
