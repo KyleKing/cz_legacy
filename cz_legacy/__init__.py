6484f5d16a32a970282d26b8bb9d7ff07347f55c
Kyle King <KyleKing@users.noreply.github.com>
2024-04-16 21:58:51 -0400

    fix: install beartype

--- cz_legacy/__init__.py
+++ cz_legacy/__init__.py
@@ -5,51 +5,54 @@ from enum import Enum
 from os import getenv
 from warnings import filterwarnings
 
-from beartype import BeartypeConf
-from beartype.claw import beartype_this_package
-from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
-from typing_extensions import Self
-
 __version__ = '1.0.1'
 __pkg_name__ = 'cz_legacy'
 
 
-class _RuntimeTypeCheckingModes(Enum):
-    """Supported global runtime type checking modes."""
+try:  # Beartype is only available on Python ^3.8.0
+    from beartype import BeartypeConf
+    from beartype.claw import beartype_this_package
+    from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+    from typing_extensions import Self
 
-    ERROR = 'ERROR'
-    WARNING = 'WARNING'
-    OFF = None
+    class _RuntimeTypeCheckingModes(Enum):
+        """Supported global runtime type checking modes."""
 
-    @classmethod
-    def from_environment(cls) -> Self:  # pragma: no cover
-        """Return the configured mode."""
-        rtc_mode = getenv('RUNTIME_TYPE_CHECKING_MODE') or None
-        try:
-            return cls(rtc_mode)
-        except ValueError:
-            modes = [_e.value for _e in cls]
-            msg = f"'RUNTIME_TYPE_CHECKING_MODE={rtc_mode}' is not an allowed mode from {modes}"
-            raise ValueError(msg) from None
+        ERROR = 'ERROR'
+        WARNING = 'WARNING'
+        OFF = None
 
+        @classmethod
+        def from_environment(cls) -> Self:  # pragma: no cover
+            """Return the configured mode."""
+            rtc_mode = getenv('RUNTIME_TYPE_CHECKING_MODE') or None
+            try:
+                return cls(rtc_mode)
+            except ValueError:
+                modes = [_e.value for _e in cls]
+                msg = f"'RUNTIME_TYPE_CHECKING_MODE={rtc_mode}' is not an allowed mode from {modes}"
+                raise ValueError(msg) from None
 
-def configure_runtime_type_checking_mode() -> None:  # pragma: no cover
-    """Optionally configure runtime type checking mode globally."""
-    rtc_mode = _RuntimeTypeCheckingModes.from_environment()
+    def configure_runtime_type_checking_mode() -> None:  # pragma: no cover
+        """Optionally configure runtime type checking mode globally."""
+        rtc_mode = _RuntimeTypeCheckingModes.from_environment()
 
-    if rtc_mode is not _RuntimeTypeCheckingModes.OFF:
-        from beartype.roar import BeartypeClawDecorWarning  # noqa: PLC0415
+        if rtc_mode is not _RuntimeTypeCheckingModes.OFF:
+            from beartype.roar import BeartypeClawDecorWarning  # noqa: PLC0415
 
-        beartype_this_package(conf=BeartypeConf(
-            warning_cls_on_decorator_exception=(
-                None if rtc_mode is _RuntimeTypeCheckingModes.ERROR else BeartypeClawDecorWarning
-            ),
-        ))
+            beartype_this_package(
+                conf=BeartypeConf(
+                    warning_cls_on_decorator_exception=(
+                        None if rtc_mode is _RuntimeTypeCheckingModes.ERROR else BeartypeClawDecorWarning
+                    ),
+                ),
+            )
 
-
-_PEP585_DATE = 2025
-if datetime.now(tz=timezone.utc).year <= _PEP585_DATE:  # pragma: no cover
-    filterwarnings('ignore', category=BeartypeDecorHintPep585DeprecationWarning)
-configure_runtime_type_checking_mode()
+    _PEP585_DATE = 2025
+    if datetime.now(tz=timezone.utc).year <= _PEP585_DATE:  # pragma: no cover
+        filterwarnings('ignore', category=BeartypeDecorHintPep585DeprecationWarning)
+    configure_runtime_type_checking_mode()
+except ImportError:
+    pass
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
